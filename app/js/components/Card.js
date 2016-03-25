4aa397fcfbaa1cb0cfae4066a853354e52639956
andela-ekupara <edwin.kupara@andela.com>
2016-03-25 21:09:20 +0300

    Add a materialize card

--- app/js/components/Card.js
+++ app/js/components/Card.js
@@ -1,47 +1,49 @@
 import React from 'react';
-require('../../styles/styles.css');
 
 export default class Layout extends React.Component {
   render() {
   	return (
-		 	<div className="row">
-	      <div className="col s12 cards-container">
-	        <div className="card transparent lighten-2">
-	          <div className="center blue-text darken-4 card-action">
-	          	<span className="card-title">Andela Kenya</span>
-	          </div>
-	          <div className="row card-action">
-		          <div className="col s6 margin-top right-border">
-		          		<img src="../../images/andelogo.png"/>
-		          </div>
-		          <div className="card-content teal-text darken-1 col s6">
-			          <div>
-			            <p>I am a very simple card. I am good at containing small bits of information.
-			            I am convenient because I require little markup to use effectively. I am a very simple card. I am good at containing small bits of information.
-			            I am convenient</p>
-			          </div>
-			          <div className="red-text">
-			          	<p>Owner:</p>
-			          	<p>Admin:</p>
-			          </div>
-		          </div>
-	          </div>
-	          <div className="card-action">
-	          	<ul>
-		            <li><a href="#" className="blue-text">More Projects</a></li>
-		            <li><a href="#" className="blue-text">Manage Users</a></li>
-		            <li><a href="#" className="blue-text">More Projects</a></li>
-		            <li><a href="#" className="blue-text">Manage Users</a></li>
-		          </ul>
-	          </div>
-	          <div className="card-action">
-	            <a href="#" className="blue-text">More Projects</a>
-	            <a href="#" className="blue-text">Manage Users</a>
-	            <a href="#" className="blue-text">Add Project</a>
-	          </div>
-	        </div>
-	      </div>
-    	</div>
+		 	<div className="no-margin row">
+        <div className="col s4 m4">
+          <div className="card teal darken-1">
+          	<div className="row">
+          		<div className="logo col s2">
+          			<img src="../../images/andelogo.png" className="avatar circle responsive-img"/>
+          		</div>
+	          	<div className="col s10 card-content white-text">
+	              <span className="card-title"><h5>Andela</h5></span>
+	              <p>Talent Accelerator Company</p>
+	            </div>
+		         </div>
+          	<div className="row">
+	            <div className="col s6 card-content white-text">
+	              <span className="card-title">Projects</span>
+							  <ul>
+						      <li>Trime Project Track</li>
+						      <li>Troupon</li>
+						      <li>Suya Bay</li>
+						      <li>VVida Video Sharing</li>
+						    </ul>
+	              <div> 
+		              <a href="#">Add New Project</a>
+		            </div>
+	            </div>
+	            <div className="col s6 card-content white-text">
+	              <span className="card-title">Users</span>
+	              <ul>
+						      <li>Godson Ukpere</li>
+						      <li>Edwin Kupara</li>
+						      <li>Koech Brian</li>
+						      <li>Kithome Jeremy</li>
+						    </ul>
+	              <div>
+		              <a href="#">Manage Users</a>
+		            </div>
+	            </div>
+	         </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
